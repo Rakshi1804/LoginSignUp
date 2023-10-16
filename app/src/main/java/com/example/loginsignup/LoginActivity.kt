586d874dc54dbf1b4561346890839bf107a42899
rams0003 <s12977.rakshith@gmail.com>
2023-10-16 15:29:12 +0530

    Profile Page with button (Separate Folder)

--- app/src/main/java/com/example/loginsignup/LoginActivity.kt
+++ app/src/main/java/com/example/loginsignup/LoginActivity.kt
@@ -33,7 +33,7 @@ class LoginActivity : AppCompatActivity() {
 
                 firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener{
                     if (it.isSuccessful){
-                        val intent = Intent(this, MainActivity::class.java)
+                        val intent = Intent(this, ProfileActivity::class.java)
                         startActivity(intent)
                     } else {
                         Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
